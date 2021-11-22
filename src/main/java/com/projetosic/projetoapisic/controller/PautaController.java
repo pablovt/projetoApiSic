2dc2829ffce3ac0ccb52ef9348967574748f4d54
Pablo Tessmann <pablo.v.tessmann@gmail.com>
2021-11-22 12:00:20 -0300

    first commit yeah

--- src/main/java/com/projetosic/projetoapisic/controller/PautaController.java
+++ src/main/java/com/projetosic/projetoapisic/controller/PautaController.java
@@ -5,12 +5,13 @@ import com.projetosic.projetoapisic.service.PautaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/pauta")
+@RequestMapping(method = RequestMethod.POST, path="/pauta")
 public class PautaController {
 
     @Autowired
